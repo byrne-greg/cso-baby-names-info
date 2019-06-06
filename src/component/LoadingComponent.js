78324b5ada000a02b6b2a430caf9dbca9a0b5b47
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-06-06 13:52:22 +0100

    fix to loading component

--- src/component/LoadingComponent.js
+++ src/component/LoadingComponent.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Spin, Skeleton, List, Card } from "antd";
 
-const loadingWithColumns = numColumns => (
-  <Spin size="large">
-    {/* <List
-      grid={{ gutter: 16, column: numColumns, size: "small" }}
-      dataSource={new Array(30)}
-      renderItem={() => (
-        <List.Item>
-          <Skeleton active>
-            <Card />
-          </Skeleton>
-        </List.Item>
-      )}
-    /> */}
-  </Spin>
-);
+const loadingWithColumns = numColumns => {
+  const dumbData = [];
+  for (let i = 0; i < 30; i++) {
+    dumbData.push({ key: i });
+  }
+
+  return (
+    <Spin size="large">
+      <List
+        grid={{ gutter: 16, column: numColumns, size: "small" }}
+        dataSource={dumbData}
+        renderItem={() => (
+          <List.Item>
+            <Skeleton active>
+              <Card />
+            </Skeleton>
+          </List.Item>
+        )}
+      />
+    </Spin>
+  );
+};
 
 export const withListPageLoading = WrappedComponent => (props = {}) => {
   const { isLoading } = props;
